9e96180e5f31ba51a1bc6e9f7456a6317dbd0e13
Name <email@email.com>
2012-12-06 02:05:19 +0100

    commit 81

--- code/createItem.php
+++ code/createItem.php
@@ -11,7 +11,7 @@
             $user->checkCookie();
 
             $design = new designclass();
-            $design->createHeader("Helping Hands > Watchlist", "css/web_tech.css");
+            $design->createHeader("Helping Hands > Create Item", "css/web_tech.css");
         ?>
     </head>
 
@@ -22,66 +22,83 @@
             ?>
 		<div id="body">
         	<div id="content">
-        <?php
-    
-    //check the user rights       
-    if (!isset($_SESSION["nick"])) {
-        echo "<meta http-equiv='refresh' content='0; url=index.php'>";
-    }
-    if (isset($_SESSION["nick"])) {
-		include 'Date.php'; // this file is needed for the date fields of the form
-        //form
-        echo"
-            <h2>Create New Event</h2>
-            Please enter all the necessary information about your event!<br><br>
-            <form action='index.php' method='post'>
-                <table border='0' cellpadding ='5'>
-                    <colgroup>
-                        <col width='100'>
-                        <col>
-                    </colgroup>
-                    <tr>
-                        <td>Title*</td>
-                        <td><input name='Title' type='text' size='30' maxlength='30'></td>
-                    </tr>
-                    <tr>
-                        <td>Type*</td>
-                        <td><input name='Type' type='text' size='30' maxlength='30'></td>
-                    </tr>
-                    <tr>
-                        <td>Date*</td>
-                        <td>"?><?php DateSelector("Sample")?><?php echo "</td>
-                    </tr>
-                    <tr>
-                        <td>Time*</td>
-                        <td><input name='Hours' type='text' size='2' maxlength='2'>:<input name='Minutes' type='text' size='2' maxlength='2'><font color='grey'><i> ex: '12:30'<i><font></td>
-                    </tr>
-                    <tr>
-                        <td>Location*</td>
-                        <td><input name='Location' type='text' size='30' maxlength='30'></td>
-                    </tr>
-                    <tr>
-                        <td>Organizer</td>
-                        <td><input name='Organizer' type='text' size='30' maxlength='30'></td>
-                    </tr>
-                    <tr>
-                        <td>Issue*</td>
-                        <td><input name='Issue' type='text' size='30' maxlength='30'></td>
-                    </tr>
-                    <tr>
-                        <td valign='top'>Description</td>
-                        <td><textarea name='Description' cols='50' rows='10'></textarea></td>
-                    </tr>
-                </table><br>
-                <input type='submit' name='createEvent_form' value='Submit'>
-                <input type='reset' value='Reset'>
-            </form><p>
-            * mandatory!</p>";
-        ?>
+		        <?php
+				    //check the user rights       
+				    if (!isset($_SESSION["nick"])) {
+				        echo "<meta http-equiv='refresh' content='0; url=index.php'>";
+				    }
+				    if (isset($_SESSION["nick"])) {
+						include 'Date.php'; // this file is needed for the date fields of the form
+				        //form
+				        echo"
+				            <h2>Create New Item</h2>
+				            Please enter all the necessary information about your item!<br><br>
+				            <form action='index.php' method='post'>
+				                <table border='0' cellpadding ='5'>
+				                    <colgroup>
+				                        <col width='100'>
+				                        <col>
+				                    </colgroup>
+				                    <tr>
+				                        <td>Type*</td>
+				                        <td><input name='Type' type='text' size='30' maxlength='30'></td>
+				                    </tr>
+				                    <tr>
+				                        <td>Title*</td>
+				                        <td><input name='Type' type='text' size='30' maxlength='30'></td>
+				                    </tr>
+									<tr>
+				                        <td>Category*</td>
+				                        <td>
+											<select name='CategoryMenu' >
+					                            <option value='Random Acts of Kindness'>Random Acts of Kindness</option>
+					                            <option value='Gifts'>Gifts</option>
+					                            <option value='Donation'>Donation</option>
+												<option value='Volunteering'>Volunteering</option>
+												<option value='Wish'>Wish</option>
+												<option value='Information'>Information</option>
+												<option value='Emergency'>Emergency</option>
+												<option value='Sharing'>Sharing</option>
+												<option value='Engagement'>Engagement</option>
+												<option value='Benefit'>Benefit</option>
+												<option value='Idea'>Idea</option>								
+					                        </select>
+										</td>
+				                    </tr>
+									<tr>
+					                    <td valign='top'>Description</td>
+					                    <td><textarea name='Description' cols='50' rows='10'></textarea></td>
+					                </tr>
+				                    <tr>
+				                        <td>Date*</td>
+				                        <td>"?><?php DateSelector("Sample")?><?php echo "</td>
+				                    </tr>
+				                    <tr>
+				                        <td>Duration</td>
+				                        <td><input name='Duration' type='text' size='30' maxlength='30'></td>
+				                    </tr>
+									<tr>
+					                    <td>Place*</td>
+					                    <td><input name='Duration' type='text' size='30' maxlength='30'></td>
+					                </tr>
+				                    <tr>
+				                        <td>E-Mail</td>
+				                        <td><input name='E-Mail' type='text' size='30' maxlength='30'></td>
+				                    </tr>
+				                    <tr>
+				                        <td>Picture</td>
+				                        <td><input name='Picture' type='text' size='100' maxlength='30'></td>
+				                    </tr>
+				                </table><br>
+				                <input type='submit' name='createEvent_form' value='Submit'>
+				                <input type='reset' value='Reset'>
+				            </form><p>
+				            * mandatory!</p>";
+			        ?>
                 </div>
                 <div id="calendar">
                     <?php
-                        include "cal.php";
+                        //include "cal.php";
                         //include "Calendar.php";
                     ?>
                 </div>
--- code/itemCreated.php
+++ code/itemCreated.php
@@ -5,6 +5,7 @@
     $db = $user->getDB();
 	var $url
     
+    //open database
     if (!$db) { 
         echo 'Error: Could not connect to database.';
         exit; 
@@ -18,33 +19,31 @@
     }
 
     // checks, if all fields were filled
-    elseif (empty($_POST['Creator'])
-            or empty($_POST['Type'])
+    elseif (empty($_POST['Type'])
+            or empty($_POST['Title'])
 			or empty($_POST['Category'])
             or empty($_POST['SampleYear']) or empty($_POST['SampleMonth']) or empty($_POST['SampleDay'])
-            or empty($_POST['Place'])
-            or empty($_POST['E-Mail'])) {
+            or empty($_POST['Place']) {
                 echo "<font color='red'>Please fill all the mandatory fields!</font><br><br>
-                <a href='./CreateEvent.php'>< Back to \"Create Event\"</a><br>";
+                <a href='./createItem.php'>< Back to \"Create Event\"</a><br>";
             }
     else {
 		if(empty($_POST['Picture']))
 			$url = 'http://yourtimematters.com.au/wp-content/uploads/2012/11/iStock_000002090601XSmall.jpg';
 		else $url = $_POST['Picture'];
 	    // fills the events table in the database
-	    $query = "INSERT INTO Events (Creator, Type, Title, Category, Description, Date, Duration, Place, E-Mail, Picture)"
+	    $query = "INSERT INTO Events (User, Type, Title, Category, Description, Date, Duration, Place, E-Mail, Picture, Postingdate)"
 	              ."VALUES (\"".$_SESSION["nick"]."\","
-	                        ."\"".$_POST['Creator']."\","
 	                        ."\"".$_POST['Type']."\","
-							."\"".$_POST['Title']."\","
-							."\"".$_POST['Category']."\","
+	                        ."\"".$_POST['Title']."\","
+							."\"".$_POST['Categroy']."\","
 							."\"".$_POST['Description']."\","
 	                        ."\"".$_POST['SampleYear']."-".$_POST['SampleMonth']."-".$_POST['SampleDay']."\","
 	                        ."\"".$_POST['Duration']."\","
 	                        ."\"".$_POST['Place']."\","
-	                        ."\"".$_POST[date("Y-m-d")]."\","
-							."\"".$_POST['E-Mail']."\","
-	                        ."\"".$_POST[$url]."\");";
+							."\"".$_POST['E-Mail']."\","						
+	                        ."\"".$_POST[$url]."\")."\","
+							."\"".$_POST[date("Y-m-d")];
 	  	  //echo $query;
 		  mysql_query($query);
    }
