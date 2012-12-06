8130091c91a13a91c7354103b65d1ba5167e40dc
Name <email@email.com>
2012-12-06 02:50:42 +0100

    commit 98

--- code/itemCreated.php
+++ code/itemCreated.php
@@ -20,14 +20,13 @@
     }
 
     // checks, if all fields were filled
-    elseif ((empty($_POST['Type'])
+    elseif (empty($_POST['Type'])
     	or empty($_POST['Title'])
 		or empty($_POST['Category'])
        	or empty($_POST['SampleYear']) or empty($_POST['SampleMonth']) or empty($_POST['SampleDay'])
         or empty($_POST['Place'])) 
          	echo "<font color='red'>Please fill all the mandatory fields!</font><br><br><a href='./createItem.php'>< Back to \"Create Event\"</a><br>";
-		
-		
+
     else {
 		if(empty($_POST['Picture']))
 			$url = 'http://yourtimematters.com.au/wp-content/uploads/2012/11/iStock_000002090601XSmall.jpg';
