4dbc7a41489b7010a8da03bef1e66e1e82f53867
Name <email@email.com>
2012-12-06 12:05:18 +0100

    commit 171

--- code/watchlistItem.php
+++ code/watchlistItem.php
@@ -44,7 +44,7 @@ session_start();
 					    if(isset($_GET["ItemId"])){
 						    $query = "INSERT INTO `Watchlist` (`User`, `ItemId`, `Date`) "
 						              ."VALUES ('".$_SESSION["nick"]."', '".$_GET["ItemId"]."', '".date("Y-m-d")."');";						
-						  	echo $query;
+						  	//echo $query;
 							mysql_query($query);
 							echo "<p><span id="add">Item successfully added to the watchlist!</span></p>";
 						}		
