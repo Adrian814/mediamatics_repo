feda11c03820a7034c3ff352524422a93f0a4cf7
Name <email@email.com>
2012-12-06 11:01:07 +0100

    commit 160

--- code/watchlistItem.php
+++ code/watchlistItem.php
@@ -45,7 +45,7 @@ session_start();
 						//add item to watchlist	   
 					    if(isset($_GET["ItemId"])){
 							echo "123";
-					        $query = "INSERT INTO `Watchlist` (`User`, `ItemId`, `Date`)"."VALUES (\"".strval($_SESSION["nick"])."\","."\"".strval ($_GET["ItemId"])."\","."\",".date("Y-m-d")."\");";
+					        $query = "INSERT INTO `Watchlist` (`User`, `ItemId`, `Date`)"."VALUES (\"".$_SESSION["nick"]."\","."\"".$_GET["ItemId"]."\","."\",".date("Y-m-d")."\");";
 			 			    mysql_query($query);
 						}
 						
