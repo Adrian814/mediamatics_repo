d67a2aa2460e8b763c4a77f1ad1cf0947cd05083
Name <email@email.com>
2012-12-06 10:11:07 +0100

    commit 139

--- code/watchlistItem.php
+++ code/watchlistItem.php
@@ -42,14 +42,14 @@ session_start();
 						
 						echo "Test";
 						//add item to watchlist	   
-					     if(isset($_GET["ItemId"])){
+				//	     if(isset($_GET["ItemId"])){
 						//	echo "Test2";
 					  	//	$query = "INSERT INTO Watchlist (User, EventId, Date)"
 					//            ."VALUES (\"".$_SESSION["nick"]."\","
 				//		  		."\"".$_Get['EventId']."\","
 					//	  		."\"".$_POST['date("Y-m-d")']."\");";
 				//	 		mysql_query($query);
-					 	  }
+					 //	  }
 						
 */
 						 // echo "<p><span id="add">Item successfully added to the watchlist!</span></p>";
