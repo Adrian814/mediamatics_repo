95d15592946f5836ca420d68656a12297feefd99
Name <email@email.com>
2012-12-15 16:26:24 +0100

    commit 217

--- code/watchlist.php
+++ code/watchlist.php
@@ -162,8 +162,8 @@ session_start();
 					
 					    // generates the list of events
 						$query1 = "SELECT * FROM `Watchlist` WHERE `User` = ".$_SESSION["nick"]." ORDER BY `Date` DESC";
-						echo $query1;
 					    $result1 = mysql_query($query1);
+						echo $result1;
 					    if ($result1) {
 					    	$num_results1 = mysql_num_rows($result1);
 						    for ($i=0; $i < $num_results1; $i++) {
