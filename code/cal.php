ccc52392df76c8b57506d9583774ef9a834aeec3
Name <email@email.com>
2012-12-18 16:29:35 +0100

    commit 231

--- code/cal.php
+++ code/cal.php
@@ -3,10 +3,10 @@
 	$result = mysql_query("SELECT * FROM `Quotes`");
 	$num_results = mysql_num_rows($result);
 	$randomId = rand(1, $num_results);
-	echo $randomId;
 	$result2 = mysql_query("SELECT * FROM `Quotes` WHERE QuoteId = '".$randomId."';");
 	$row = mysql_fetch_array($result2);
 	$quote = $row['Text'];
 	echo $quote;
-	echo 'Test';
+	echo '<br><br>
+		  <a href="index.php"><img src="http://unitedwaycalvert.org/images/banner160x370-Side-Banner-2.jpg" alt="banner" style="border-style:none"></a>';
 ?>
