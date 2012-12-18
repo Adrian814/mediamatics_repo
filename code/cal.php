6498b2b0b779605cdc3d82b7e70938ca4164d2d4
Name <email@email.com>
2012-12-18 16:17:13 +0100

    commit 230

--- code/cal.php
+++ code/cal.php
@@ -3,6 +3,7 @@
 	$result = mysql_query("SELECT * FROM `Quotes`");
 	$num_results = mysql_num_rows($result);
 	$randomId = rand(1, $num_results);
+	echo $randomId;
 	$result2 = mysql_query("SELECT * FROM `Quotes` WHERE QuoteId = '".$randomId."';");
 	$row = mysql_fetch_array($result2);
 	$quote = $row['Text'];
