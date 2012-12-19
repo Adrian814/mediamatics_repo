db02c1e86e09346728ab49d725259bb98710dfd3
Name <email@email.com>
2012-12-19 13:51:51 +0100

    commit 262

--- code/cal.php
+++ code/cal.php
@@ -1,14 +1,14 @@
 <?php
 	// generates the quote
-	$result = mysql_query("SELECT * FROM `Quotes`");
+	$result = mysql_query('SELECT * FROM `Quotes`');
 	$num_results = mysql_num_rows($result);
 	$randomId = rand(1, $num_results);
-	$result2 = mysql_query("SELECT * FROM `Quotes` WHERE QuoteId = '".$randomId."';");
+	$result2 = mysql_query('SELECT * FROM `Quotes` WHERE QuoteId = ''.$randomId.'';');
 	$row = mysql_fetch_array($result2);
 	$quote = $row['Text'];
 	echo $quote;
 	echo '<br><br>
-		  <a href="index.php"><img src="http://unitedwaycalvert.org/images/banner160x370-Side-Banner-2.jpg" alt="banner" style="border-style:none"></a>';
+		  <a href='index.php'><img src='http://unitedwaycalvert.org/images/banner160x370-Side-Banner-2.jpg' alt='banner' style='border-style:none'></a>';
 	echo "<br><br>
 		  <script language='javascript'type='text/javascript'>function FG_4ccb04fb46c30_loadScript(){var fgScriptUrl='http://donate.firstgiving.com/dpa/static/js/site/fg_consumer_donation_opener.min.js';var script=document.createElement('script');script.type='text/javascript';if(script.readyState){script.onreadystatechange=function(){if(script.readyState=='loaded'||script.readyState=='complete'){script.onreadystatechange=null}}}else{script.onload=function(){}}script.src=fgScriptUrl;document.body.appendChild(script)}FG_4ccb04fb46c30_loadScript();</script></script> 
 
