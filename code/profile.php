af0f352df9a29f7dedf2470582f758abfc1a8ec0
Name <email@email.com>
2012-12-20 16:01:38 +0100

    commit 298

--- code/profile.php
+++ code/profile.php
@@ -11,13 +11,21 @@
         $user->checkCookie();
 
         $design = new designclass();
-        $design->createHeader("Helping Hands > User > Profile", "css/web_tech.css");
-        ?>      
+        $design->createHeader("Demo Kalender > Settings", "css/web_tech.css");
+
+        
+
+        ?>
+
+        
     </head>
     <body>
         <div id="all">
+
             <?php
-	            $design->createNavigation("navigation", "navigation_logo", "navigation_title", "navigation_nav", "nav", "navigation_login");
+
+            $design->createNavigation("navigation", "navigation_logo", "navigation_title", "navigation_nav", "nav", "navigation_login");
+
             ?>
             <div id="body">
                 <div id="content">
@@ -26,18 +34,21 @@
                             echo "<meta http-equiv='refresh' content='0; url=index.php'>";
                         }
                         else
-                            $user->createSettings($_GET["Nick"], false);
+                            $user->createSettings($_GET["nick"], false);
+
                     if(isset($msg)) {
                         echo "<span id='error'>";
                         echo $msg;
                         echo "</span>";
                     }
                     ?>
+
+
+
+
                 </div>
                 <div id="calendar">
-                    <?php 
-						include 'cal.php'; 
-					?>
+                    <?php include "cal.php"; ?>
                 </div>
             </div>
         </div>
