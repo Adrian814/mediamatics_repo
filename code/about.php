2d615f7545ce26524983e839e7afa7b21f8b7974
Name <email@email.com>
2012-12-20 15:11:37 +0100

    commit 291

--- code/about.php
+++ code/about.php
@@ -11,16 +11,27 @@
             $user->checkCookie();
 
             $design = new designclass();
-            $design->createHeader("Demo Kalender > About", "css/web_tech.css");
-
-
+            $design->createHeader("Helping Hands > About", "css/web_tech.css");
         ?>
 
     </head>
     <body>
         <div id="all">
-
             <?php
+				//connect to database
+				$db = mysql_connect("a.db.shared.orchestra.io", "user_6b0d5c75", "bTh4cEKXeAtx!o") or die(mysql_error());
+				mysql_select_db("db_6b0d5c75", $db) or die(mysql_error());
+
+			    if (!$db) {
+			        echo 'Error: Could not connect to database.';
+			        exit;
+			    }
+
+			    $mysql = mysql_select_db('db_6b0d5c75');
+			    if(!$mysql) {
+			        echo 'Cannot select database.';
+			        exit;
+				}
 
                 $design->createNavigation(  "navigation",
                                             "navigation_logo",
@@ -33,18 +44,12 @@
             ?>
             <div id="body">
                 <div id="content">
-                    <h2>About</h2>
-                     Platform for help services (demand and request)
-                     
-                     <p>This website was written by Adrian Siuda.</p>
-
-                     <p>
-                         Comments and remarks can be sent to the <a href="mailto:democal2009@gmail.com">admin</a>.
-                     </p>
-
+                    <h2>About</h2>              
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 		accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                 </div>
                 <div id="calendar">
                     <?php
+						include 'cal.php';
                     ?>
                 </div>
             </div>
