ff9e20f767b51bdeb5a8196296663021db628317
Name <email@email.com>
2012-12-20 14:42:11 +0100

    commit 279

--- code/index.php
+++ code/index.php
@@ -216,7 +216,7 @@ session_start();
 	            </div>
 	    		<div id="calendar">
 	    			<?php
-	        			include 'cal.php';
+	        			include 'sidebar.php';
 	       			?>
 	    		</div>
 	  		</div>
